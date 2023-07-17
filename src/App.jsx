d4e044063cc6bcbda3d2c68164842a4ad40c630a
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2023-07-16 21:37:32 -0600

    Add background image and color

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ function App() {
 
   return (
 
-    <>
-      
-    </>
+    <div className="">
+      <main className='bg-white dark:bg-slate-202 min-h-[100vh]'>
+        <div className='bg-[url("public/images/bg-mobile-light.jpg")] md:bg-[url("public/images/bg-desktop-light.jpg")] bg-cover bg-center h-[200px] md:h-[300px] w-[100%] dark:bg-[url("public/images/bg-mobile-dark.jpg")] dark:md:bg-[url("public/images/bg-desktop-dark.jpg")]'>
+        </div>
+      </main>
+    </div>
   )
 } 
 
