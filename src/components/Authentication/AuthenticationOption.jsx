e02a39a27543756dfa3ae968c99b112bfdfa0a82
Alberto Ledesma <alberto.ledesma.dev@gmail.com>
2023-11-02 22:05:16 -0600

    Change the font size and font family to Josefin Sans

--- src/components/Authentication/AuthenticationOption.jsx
+++ src/components/Authentication/AuthenticationOption.jsx
@@ -11,7 +11,7 @@ function AuthenticationOption() {
     const bgImage = `bg-[url("./assets/bg-auth-mobile.jpg")] bg-center bg-cover lg:bg-[url("./assets/bg-auth-desktop.jpg")]`
  
     return(
-        <main className="min-h-[100vh] bg-auth-bg text-white flex flex-col justify-center items-center">
+        <main className="font-jos min-h-[100vh] bg-auth-bg text-white flex flex-col justify-center items-center">
             {/* Card */}
             <div className="flex flex-col w-[90%] max-w-[630px] min-h-[640px] max-h-[754px] h-[90vh] my-5 bg-auth-card rounded-2xl shadow-lg shadow-grayish-104 lg:flex-row-reverse lg:h-[700px] lg:max-w-[1160px]">
                 {/* H1 and Background image */}
@@ -25,21 +25,21 @@ function AuthenticationOption() {
                     <h1 className="uppercase text-auth-slate text-7xl font-bold tracking-[.1em] text-center mb-[60px] hidden lg:block">Todo</h1>
                     {/* Login Button */}
                     <Button 
-                    className="bg-auth-blue text-white mb-4 shadow-md shadow-grayish-104 hover:bg-auth-blue-h transition-colors lg:text-lg"
+                    className="bg-auth-blue text-white mb-4 shadow-md shadow-grayish-104 hover:bg-auth-blue-h transition-colors text-lg"
                     onClick={selectLogin}>Login</Button>
                     {/* Sign Up button */}
                     <Button 
-                    className="border-auth-blue text-auth-blue border-[2px] p-2.5 hover:border-auth-blue-h hover:text-auth-blue-h transition-colors lg:text-lg" 
+                    className="border-auth-blue text-auth-blue border-[2px] p-2.5 hover:border-auth-blue-h hover:text-auth-blue-h transition-colors text-lg" 
                     bgVoid={true}
                     onClick={selectSignUp}>Sign Up</Button>
                     {/* Or text */}
                     <div className="flex justify-center items-center">
                         <div className="w-20 h-[2px] bg-auth-blue"></div>
-                        <span className="mx-5 my-7 text-auth-gray text-xs lg:text-base">Or</span>
+                        <span className="mx-5 my-7 text-auth-gray text-base">Or</span>
                         <div className="w-20 h-[2px] bg-auth-blue"></div>
                     </div>
                     {/* Continue with Google */}
-                    <Button className="flex items-center justify-center text-auth-slate border-auth-slate border-[2px] p-2.5 hover:border-auth-slate-h hover:text-auth-slate-h transition-colors font-normal lg:text-lg" 
+                    <Button className="flex items-center justify-center text-auth-slate border-auth-slate border-[2px] p-2.5 hover:border-auth-slate-h hover:text-auth-slate-h transition-colors font-normal text-lg" 
                     bgVoid={true}>
                         <FaGoogle className="text-xl" /> 
                         <span className="ml-2">Continue with Google</span>
